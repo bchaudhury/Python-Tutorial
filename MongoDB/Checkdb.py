8e7de09bd558b89049752c4ddbc820dfa9849375
BHASKAR CHAUDHURY <chaudhury.bhaskar@gmail.com>
2025-02-27 19:49:58 +0530

    27/2/25 Revised Commit

--- MongoDB/Checkdb.py
+++ MongoDB/Checkdb.py
@@ -3,7 +3,7 @@ import pymongo
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 
 dblist = myclient.list_database_names()
-if "mydatabase" in dblist:
+if "MyDatabase" in dblist:
   print("The database exists.")
 
 
