321307b408c68414d3cc7f07a3d93c2c1bfec511
BHASKAR CHAUDHURY <chaudhury.bhaskar@gmail.com>
2025-02-28 17:03:05 +0530

    28/2/25 Commit Rev1

---
+++ __pycache__/General/AverageSpeed.py
@@ -0,0 +1,8 @@
+import numpy
+
+# Calculate the average speed of 13 cars as recorded in the list below
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = numpy.mean(speed)
+
+print("The average speed is: " , x)
